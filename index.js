cd9eaa14d92773862ae69c389e518c22f5eccc8c
_ToOOom <63470060+Yellow-Submarine215@users.noreply.github.com>
2020-04-27 17:55:06 +0200

    All is finished

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const colors = require('colors');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+console.log('TOKEN:'.underline.yellow, token)
+console.log('Chargement...')
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Le bot est en ligne!'.green);
 });
 
-client.login('your-token-goes-here');
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
+client.login(token);
